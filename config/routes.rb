09e98a9cad8496beb46cd71bcb923bff1fc80485
Aime Malaika <aimemalaika1995@gmail.com>
2022-04-27 18:09:00 +0200

    Setup routes and controller for users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'users/' => 'users#index'
+  get 'users/:id' => 'users#show'
 
+  get 'posts/' => 'posts#index'
+  get 'posts/:id' => 'posts#show'
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
