c87c05218c48b37e3c4270911970e794e317f802
Aime Malaika <aimemalaika1995@gmail.com>
2022-04-29 19:21:09 +0200

    Add controller test

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'users/' => 'users#index'
-  get 'users/:id' => 'users#show'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+  
+  # get 'users/' => 'users#index'
+  # get 'users/:id' => 'users#show'
 
-  get 'users/:user_id/posts' => 'posts#index'
-  get 'users/:user_id/posts/:id' => 'posts#show'
+  # get 'users/:user_id/posts' => 'posts#index'
+  # get 'users/:user_id/posts/:id' => 'posts#show'
   # Defines the root path route ("/")
   root 'users#index'
 end
