0fbab0bee5e802f928ff438b077010051cffe22d
Aime Malaika <aimemalaika1995@gmail.com>
2022-05-07 07:28:10 +0200

    Added like and comment post

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show, :new, :create] do
-    resources :posts, only: [:index, :show, :new, :create]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
   end
-  
-  # get 'users/' => 'users#index'
-  # get 'users/:id' => 'users#show'
 
-  # get 'users/:user_id/posts' => 'posts#index'
-  # get 'users/:user_id/posts/:id' => 'posts#show'
-  # Defines the root path route ("/")
   root 'users#index'
 end
