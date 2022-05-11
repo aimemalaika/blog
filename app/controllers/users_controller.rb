1200f6d3afe4f68a4bee44303183a36536c15017
Aime Malaika <aimemalaika1995@gmail.com>
2022-05-11 13:26:30 +0200

    Add css to all forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,21 +8,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  # def new
-  #   @user = User.new
-  # end
-
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     redirect_to @user
-  #   else
-  #     render 'new'
-  #   end
-  # end
-
-  # def user_params
-  #   params.require(:user).permit(:Name, :Photo, :Bio)
-  # end
+  
 end
