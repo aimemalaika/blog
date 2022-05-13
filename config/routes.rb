557bea5d208a3bcc09f30044837ede2571e3bd1d
Aime Malaika <aimemalaika1995@gmail.com>
2022-05-13 18:56:45 +0200

    Add user list and user per id end points

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users, controllers: do
+  #   sessions: 'api/v1/sessions',
+  #   registrations: 'api/v1/registrations',
+  # end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:index, :show, :create, :new, :destroy] do
+          resources :comments, only: [:create, :new, :destroy]
+          resources :likes, only: [:create]
+        end
+      end
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # add new separate route as ressource
   resources :home, only: [:index]
